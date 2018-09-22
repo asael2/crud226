22ff0b570439dbb8eead670d3b13e928af46288b
asael <asael2@gmail.com>
2018-09-21 22:00:10 -0500

    Rest on

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  
+  namespace 'api' do
+    namespace 'v1' do
+      resources :articles
+    end
+  end
+
+  post 'user_token' => 'user_token#create'
+  get 'users/current' => 'users#current'
+  resources :users
+
+end
